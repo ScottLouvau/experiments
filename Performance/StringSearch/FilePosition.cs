443ff1376dfb0ee48b0f21005e666464801650c2
Scott Louvau <slouvau@hotmail.com>
2021-05-18 22:53:53 -0700

    Vector newline counting (experiment)

--- Performance/StringSearch/FilePosition.cs
+++ Performance/StringSearch/FilePosition.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace StringSearch
 {
@@ -58,6 +60,7 @@ namespace StringSearch
             current.ByteOffset += content.Length;
             current.CharOffset = 0;
 
+            // Count lines to the end of the buffer
             while (true)
             {
                 int nextNewline = content.IndexOf((byte)'\n');
@@ -69,6 +72,38 @@ namespace StringSearch
                 content = content.Slice(nextNewline + 1);
             }
 
+            // Find the CharInLine at the end of the buffer
+            int continuations = 0;
+            for (int i = 0; i < content.Length; ++i)
+            {
+                if (content[i] >= 0x80 && content[i] < 0xC0) { continuations++; }
+                //if (content[i] - 0x80 < 0x40) { continuations++; }
+            }
+
+            // Or, to match .NET char count rather than UTF-8 codepoint count:
+            //int dotNetCount = Encoding.UTF8.GetCharCount(content);
+            //Debug.Assert(content.Length - continuations == dotNetCount);
+
+            current.CharInLine += content.Length - continuations;
+            return current;
+        }
+
+        public static FilePosition Update2(FilePosition start, ReadOnlySpan<byte> content)
+        {
+            FilePosition current = start;
+
+            current.ByteOffset += content.Length;
+            current.CharOffset = 0;
+
+            // Count lines to the end of the buffer
+            current.LineNumber += Vector.NewlineCount(content, out int lastLineIndex);
+
+            if (lastLineIndex > 0)
+            {
+                content = content.Slice(lastLineIndex + 1);
+            }
+
+            // Find the CharInLine at the end of the buffer
             int continuations = 0;
             for (int i = 0; i < content.Length; ++i)
             {
---
+++ Performance/StringSearch/Vector.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace StringSearch
+{
+    public unsafe static class Vector
+    {
+        private static Vector256<sbyte> SetVector256To(byte value)
+        {
+            sbyte* _loader = stackalloc sbyte[32];
+            for (int i = 0; i < 32; ++i)
+            {
+                _loader[i] = unchecked((sbyte)value);
+            }
+
+            return Unsafe.Read<Vector256<sbyte>>(_loader);
+        }
+
+        private static Vector128<sbyte> SetVector128To(byte value)
+        {
+            sbyte* _loader = stackalloc sbyte[16];
+            for (int i = 0; i < 16; ++i)
+            {
+                _loader[i] = unchecked((sbyte)value);
+            }
+
+            return Unsafe.Read<Vector128<sbyte>>(_loader);
+        }
+
+        public static int NewlineCount(ReadOnlySpan<byte> content, out int lastNewlineIndex)
+        {
+            lastNewlineIndex = -1;
+            int newlineCount = 0;
+
+            int i = 0;
+            int fullBlockLength = content.Length - 32;
+
+            if (i < fullBlockLength)
+            {
+                fixed (byte* contentPtr = &content[0])
+                {
+                    Vector256<sbyte> newlineV = SetVector256To((byte)'\n');
+
+                    for (; i < fullBlockLength; i += 32)
+                    {
+                        // Load a vector of bytes
+                        Vector256<sbyte> contentV = Unsafe.ReadUnaligned<Vector256<sbyte>>(&contentPtr[i]);
+
+                        // Find newlines and convert to a bit vector
+                        Vector256<sbyte> matches = Avx2.CompareEqual(newlineV, contentV);
+                        uint newlineBits = unchecked((uint)Avx2.MoveMask(matches));
+
+                        // Count the newlines seen
+                        int lineCount = (int)Popcnt.PopCount(newlineBits);
+                        newlineCount += lineCount;
+
+                        if (lineCount > 0)
+                        {
+                            lastNewlineIndex = i + (int)Bmi1.TrailingZeroCount(newlineBits);
+                        }
+                    }
+                }
+            }
+
+            // Count newlines after the end of the block
+            for (; i < content.Length; ++i)
+            {
+                if (content[i] == (byte)'\n')
+                {
+                    newlineCount++;
+                    lastNewlineIndex = i;
+                }
+            }
+
+            return newlineCount;
+        }
+    }
+}
