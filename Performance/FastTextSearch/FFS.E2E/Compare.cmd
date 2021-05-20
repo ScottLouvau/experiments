d8547928ca18cb85519708c430d24daeda00cea2
Scott Louvau <slouvau@hotmail.com>
2021-05-20 15:05:32 -0700

    Trick: Compare logs to skipped file extension to avoid lock on output log issue.

--- Performance/FastTextSearch/FFS.E2E/Compare.cmd
+++ Performance/FastTextSearch/FFS.E2E/Compare.cmd
@@ -6,15 +6,15 @@ SET FFS="%~dp0..\FFS\bin\Release\net5.0\FFS.exe"
 
 %FFS% Convert C:\Code\bion > nul
 ECHO.
-%FFS% Convert C:\Code\bion "*.*" Log\Bion.Convert.Utf8.log Utf8 20
+%FFS% Convert C:\Code\bion "*.*" Log\Bion.Convert.Utf8.v2 Utf8 20
 ECHO.
-%FFS% Convert C:\Code\bion "*.*" Log\Bion.Convert.DotNet.log DotNet 20
+%FFS% Convert C:\Code\bion "*.*" Log\Bion.Convert.DotNet.v2 DotNet 20
 
 %FFS% Console.WriteLine C:\Code > nul
 ECHO.
-%FFS% Console.WriteLine C:\Code "*.*" Log\Console.WriteLine.Code.Utf8.log Utf8
+%FFS% Console.WriteLine C:\Code "*.*" Log\Console.WriteLine.Code.Utf8.v2 Utf8
 ECHO.
-%FFS% Console.WriteLine C:\Code "*.*" Log\Console.WriteLine.Code.DotNet.log DotNet
+%FFS% Console.WriteLine C:\Code "*.*" Log\Console.WriteLine.Code.DotNet.v2 DotNet
 ECHO.
 
 
