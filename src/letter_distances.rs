7576b14ad9143036af11e064d8dc2921ebcced50
Scott Louvau <slouvau@hotmail.com>
2024-02-01 15:45:40 -0800

    Quantized letter distance. Word distance (digit per letter). Build map of distance -> answers for a guess. Find best guess by distinct answer count. Show cluster vector of a guess.

--- src/letter_distances.rs
+++ src/letter_distances.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 
 pub const LETTER_POSITIONS: [(u16, u16); 26] = [
     (31, 72),
@@ -37,4 +39,134 @@ pub fn distance_between_letters(mut left: char, mut right: char) -> f64 {
 
     let (dx, dy) = (x2 as f64 - x1 as f64, y2 as f64 - y1 as f64);
     (dx * dx + dy * dy).sqrt()
+}
+
+pub fn distance_between_letters_quantized(left: char, right: char) -> u8 {
+    let dist = distance_between_letters(left, right);
+    (dist / 50.0).round() as u8
+}
+
+pub fn word_distance(left: &str, right: &str) -> u32 {
+    let mut distance = 0u32;
+
+    for (l, r) in left.chars().zip(right.chars()) {
+        distance *= 10;
+        distance += distance_between_letters_quantized(l, r) as u32;
+    }
+
+    distance
+}
+
+pub fn score_to_digits(score: u32) -> Vec<u8> {
+    let mut digits = Vec::new();
+    let mut left = score;
+
+    while left > 0 {
+        digits.push((left % 10) as u8);
+        left /= 10;
+    }
+
+    digits.reverse();
+    digits
+}
+
+pub fn load_answers<'a>(text: &'a String) -> Vec<&'a str> {
+    let mut answers = Vec::new();
+
+    for line in text.lines() {
+        answers.push(line);
+    }
+
+    answers
+}
+
+pub fn word_distance_map<'a>(guess: &str, answers: &[&'a str]) -> HashMap<u32, Vec<&'a str>> {
+    let mut map: HashMap<u32, Vec<&str>> = HashMap::new();
+
+    for answer in answers {
+        let distance = word_distance(guess, answer);
+        let entry = map.entry(distance);
+        entry.or_insert(Vec::new()).push(*answer);
+    }
+
+    map
+}
+
+pub fn map_to_cv(map: &HashMap<u32, Vec<&str>>) -> Vec<u32> {
+    let mut cv = Vec::new();
+
+    for (_, answers) in map {
+        let count = answers.len();
+        while cv.len() < count {
+            cv.push(0);
+        }
+
+        cv[count - 1] += 1;
+    }
+
+    cv
+}
+
+pub fn cv_to_string(cv: &[u32]) -> String {
+    let mut result = String::new();
+    result += "[";
+
+    for (i, count) in cv.iter().enumerate() {
+        if i > 0 {
+            result += ", ";
+        }
+        result += &format!("{}", count);
+    }
+
+    result += "]";
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use super::*;
+
+    #[test]
+    fn letter_distances() {
+        for l in 'a'..='z' {
+            assert_eq!(0, distance_between_letters_quantized(l, l));
+        }
+
+        assert_eq!(1, distance_between_letters_quantized('O', 'p'));
+        assert_eq!(1, distance_between_letters_quantized('l', 'p'));
+        assert_eq!(2, distance_between_letters_quantized('K', 'P'));
+        assert_eq!(3, distance_between_letters_quantized('m', 'P'));
+
+        assert_eq!(4, distance_between_letters_quantized('g', 'a'));
+    }
+
+    #[test]
+    fn word_distances() {
+        assert_eq!(0, word_distance("hello", "hello"));
+        assert_eq!(42521, word_distance("apple", "vivid"));
+    }
+
+    #[test]
+    fn score_to_digits_test() {
+        assert_eq!(vec![1, 2, 3], score_to_digits(123));
+        assert_eq!(vec![1, 0, 0, 0], score_to_digits(1000));
+        assert_eq!(vec![3, 5, 4, 6, 9], score_to_digits(35469));
+    }
+
+    #[test]
+    fn map_and_cv() {
+        let text = fs::read_to_string("answers.txt").unwrap();
+        let answers = load_answers(&text);
+        assert_eq!(2315, answers.len());
+
+        let map = word_distance_map("apple", &answers);
+        assert_eq!(2315, map.iter().map(|(_, v)| v.len()).sum::<usize>());
+
+        let cv = map_to_cv(&map);
+        assert_eq!(vec![1914, 162, 23, 2], cv);
+
+        let cv = cv_to_string(&cv);
+        assert_eq!("[1914, 162, 23, 2]", cv);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,179 @@
+use std::{env, fs, process::ExitCode};
+use crate::letter_distances::*;
+
 mod letter_distances;
 
-fn main() {
+// Goals:
+//  - Compute letter distances accurately.
+//  - Show letter options for guess and distances (qwertle options apple 46294)
+//  - Compute distances between guess and all answer options
+//  - Show answer buckets for a given guess.
+//  - ? Draw keyboard with possible letters colored?
+
+const USAGE: &str = "Usage: qwertle <mode> <args>
+  letter <letter>: Show distance of each letter from <letter>
+  word <guess>: Show answer distances from guess
+  best: Find guess with the most distinct responses
+  cv <guess>: Show cluster vector of guess
+  options <guess> <score>: Show letter possibles for score";
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    let mut args = &args[1..];
+
+    if args.len() < 1 {
+        return print_usage("No mode provided.");
+    }
+
+    let text = fs::read_to_string("answers.txt").unwrap();
+    let text = text.to_ascii_lowercase();
+    let answers = load_answers(&text);
+
+    let mode = args.first().unwrap().to_ascii_lowercase();
+    let mode = mode.as_str();
+    args = &args[1..];
+
+    match mode {
+        "letter" => {
+            if args.len() == 0 {
+                return print_usage("'letter' not provided.");
+            }
+
+            let from: char = args[0].chars().next().unwrap();
+            for (_, to, distance_round, distance_exact) in distances_from_letter(from) {
+                println!("{from}{to} -> {distance_round} ({distance_exact:.0})");
+            }
+        },
+
+        "word" => {
+            if args.len() == 0 {
+                return print_usage("'word' not provided.");
+            }
+
+            let guess = args[0].to_ascii_lowercase();
+            println!("Answer Distances from '{guess}':\n");
+
+            let set = distances_from_guess(&guess, &answers);
+            for (answer, distance) in set.iter() {
+                println!("{distance:05} -> {answer}");
+            }
+
+            let distinct_distances = distinct_distances(&set);
+            println!("\n{distinct_distances} distinct distances.");
+        },
+
+        "best" => {
+            let mut best = None;
+
+            // let text = fs::read_to_string("guesses.txt").unwrap();
+            // let text = text.to_ascii_lowercase();
+            // let guesses = load_answers(&text);
+            let guesses = &answers;
+
+            for guess in guesses.iter() {
+                let set = distances_from_guess(&guess, &answers);
+                let distinct_distances = distinct_distances(&set);
+
+                if distinct_distances >= 2100 {
+                    println!("{distinct_distances}: {guess}");
+                }
+
+                if distinct_distances > best.unwrap_or(("", 0)).1 {
+                    best = Some((&guess, distinct_distances));
+                }
+            }
+
+            if let Some((guess, distinct_distances)) = best {
+                println!("Best: {guess} ({distinct_distances} distinct distances).");
+            }
+        },
+
+        "cv" => {
+            if args.len() == 0 {
+                return print_usage("'word' not provided.");
+            }
+
+            let guess = args[0].to_ascii_lowercase();
+            let map = letter_distances::word_distance_map(&guess, &answers);
+            let cv = map_to_cv(&map);
+            let cv = cv_to_string(&cv);
+            println!("{}", cv);
+        },
+
+        "options" => {
+            if args.len() < 2 {
+                return print_usage("'score' not provided.");
+            }
+            
+            let guess = args[0].to_ascii_lowercase();
+            let score = args[1].parse::<u32>().unwrap();
+            let score_digits = score_to_digits(score);
+
+            for (letter, distance) in guess.chars().zip(score_digits.iter()) {
+
+                for option in 'a'..='z' {
+                    let distance_round = letter_distances::distance_between_letters_quantized(letter, option);
+                    if distance_round == *distance {
+                        print!("{option}");
+                    }
+                }
+
+                print!("\t");
+            }
+        }
+
+        _ => {
+            return print_usage(&format!("Unknown mode: {}", mode));
+        }
+    }
+
+    ExitCode::SUCCESS
+}
+
+fn print_usage(error: &str) -> ExitCode {
+    println!("Error: {}", error);
+    println!("{}", USAGE);
+    ExitCode::FAILURE
+}
+
+fn distances_from_letter(letter: char) -> Vec<(char, char, u8, f64)>{
     let mut set = Vec::new();
 
-    let left = 'p';
     for right in 0u8..26 {
         let right = (right + b'A') as char;
-        let dist = letter_distances::distance_between_letters(left, right);
-        set.push((left, right, dist));
+        let distance_round = letter_distances::distance_between_letters_quantized(letter, right);
+        let distance_exact = letter_distances::distance_between_letters(letter, right);
+        set.push((letter, right, distance_round, distance_exact));
     }
 
-    set.sort_by(|a, b| a.2.partial_cmp(&b.2).unwrap());
-    for (left, right, dist) in set {
-        println!("{left}{right} => {dist:.0}");
+    set.sort_by(|a, b| a.3.partial_cmp(&b.3).unwrap());
+    set
+}
+
+fn distances_from_guess<'a>(guess: &str, answers: &Vec<&'a str>) -> Vec<(&'a str, u32)> {
+    let mut set = Vec::new();
+
+    for answer in answers {
+        let distance = word_distance(guess, *answer);
+        set.push((*answer, distance));
     }
+
+    set.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+    set
+}
+
+fn distinct_distances(set: &Vec<(&str, u32)>) -> u32 {
+    let mut last_distance = 0;
+    let mut distinct_distances = 0;
+
+    for (_, distance) in set {
+        if *distance > last_distance {
+            last_distance = *distance;
+            distinct_distances += 1;
+        } else if *distance < last_distance {
+            panic!("Distances must be sorted!");
+        }
+    }
+
+    distinct_distances
 }
